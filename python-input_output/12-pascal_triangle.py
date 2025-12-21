a29b148e6da582f8d53eef8c319afcda4997a830
DevAyyub <ayyub.mammadov.2005@gmail.com>
2025-12-21 21:22:44 +0000

    12. Pascal's Triangle

---
+++ python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""
+This module defines a function for generating Pascal's triangle.
+"""
+
+
+def pascal_triangle(n):
+    """
+    Returns a list of lists of integers representing the Pascal’s triangle of n.
+    """
+    if n <= 0:
+        return []
+
+    triangle = [[1]]
+    while len(triangle) != n:
+        tri = triangle[-1]
+        tmp = [1]
+        for i in range(len(tri) - 1):
+            tmp.append(tri[i] + tri[i + 1])
+        tmp.append(1)
+        triangle.append(tmp)
+    return triangle
