c8dd0104c737ff7535cf8a4cf15e633a2904e7df
DevAyyub <ayyub.mammadov.2005@gmail.com>
2025-12-21 21:03:46 +0000

    0. Read file

---
+++ python-inheritance/holbertonschool-higher_level_programming/python-input_output/0-read_file.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""
+This module defines a text file-reading function.
+"""
+
+
+def read_file(filename=""):
+    """
+    Reads a text file (UTF8) and prints it to stdout.
+    """
+    with open(filename, "r", encoding="utf-8") as f:
+        print(f.read(), end="")
