dd9ffeec83d54da95c1e45b35bb64da54c6ca648
DevAyyub <ayyub.mammadov.2005@gmail.com>
2025-12-21 20:41:24 +0000

    Fix Task 0

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -8,10 +8,10 @@ def lookup(obj):
     """
     Returns the list of available attributes and methods of an object.
     """
-    return dir(obj)#!/usr/bin/python3
-"""Module for lookup function."""
-
-
-def lookup(obj):
-    """Returns the list of available attributes and methods of an object."""
     return dir(obj)
+
+
+
+
+
+
