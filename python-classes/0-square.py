a6979d768c018e7ab503cd8508d42669c945b28f
DevAyyub <ayyub.mammadov.2005@gmail.com>
2025-12-21 18:25:31 +0000

    Task 0: My first square

---
+++ python-classes/0-square.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Module that defines a Square class"""
+
+
+class Square:
+    """Class that defines a square"""
+    def __init__(self, size):
+        """Initialize a new Square instance
+        Args:
+            size (int): The size of the square
+        """
+        self.__size = size
