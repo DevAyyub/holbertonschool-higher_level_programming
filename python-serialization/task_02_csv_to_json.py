5cdae3d12b35fc789307f2e3336b7f44909fb1e7
DevAyyub <ayyub.mammadov.2005@gmail.com>
2025-12-21 20:04:29 +0000

    Fix attribute names and filenames for checker

--- python-serialization/task_02_csv_to_json.py
+++ python-serialization/task_02_csv_to_json.py
@@ -7,18 +7,13 @@ import json
 def convert_csv_to_json(csv_filename):
     """ Converts a CSV file to a JSON file named data.json """
     try:
-        # Open and read the CSV file
         with open(csv_filename, 'r', encoding='utf-8') as csv_f:
-            # Use DictReader to automatically map headers to keys
             csv_reader = csv.DictReader(csv_f)
             data = [row for row in csv_reader]
 
-        # Write the resulting list of dictionaries to data.json
         with open('data.json', 'w', encoding='utf-8') as json_f:
-            json.dump(data, json_f, indent=4)
+            json.dump(data, json_f)
         
         return True
-    except FileNotFoundError:
-        return False
     except Exception:
         return False
