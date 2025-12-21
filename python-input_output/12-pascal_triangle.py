cf78c31195e4b9a5ca08fe7c242759af62c5a52b
DevAyyub <ayyub.mammadov.2005@gmail.com>
2025-12-21 21:38:48 +0000

    Fix PEP8 line length

--- python-input_output/12-pascal_triangle.py
+++ python-input_output/12-pascal_triangle.py
@@ -6,7 +6,8 @@ This module defines a function for generating Pascal's triangle.
 
 def pascal_triangle(n):
     """
-    Returns a list of lists of integers representing the Pascal’s triangle of n.
+    Returns a list of lists of integers representing the Pascal’s
+    triangle of n.
     """
     if n <= 0:
         return []
