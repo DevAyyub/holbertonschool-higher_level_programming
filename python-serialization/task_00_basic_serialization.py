1784b61fd9c84cd94f8d41d469282de32c5fe56c
DevAyyub <ayyub.mammadov.2005@gmail.com>
2025-12-21 19:59:20 +0000

    Fix logic for basic serialization

--- python-serialization/task_00_basic_serialization.py
+++ python-serialization/task_00_basic_serialization.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
+""" Module for basic serialization """
 import json
 
+
 def serialize_and_save_to_file(data, filename):
-    """Serialize a Python dictionary to a JSON file."""
-    with open(filename, 'w') as f:
+    """ Serializes a dictionary and saves it to a JSON file """
+    with open(filename, 'w', encoding='utf-8') as f:
         json.dump(data, f)
 
+
 def load_and_deserialize_from_file(filename):
-    """Deserialize a JSON file to a Python dictionary."""
-    with open(filename, 'r') as f:
+    """ Loads a JSON file and deserializes it to a dictionary """
+    with open(filename, 'r', encoding='utf-8') as f:
         return json.load(f)
