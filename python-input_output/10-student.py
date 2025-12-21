c420b4ae5f304984548ab8a1744a63b01dc3c4a1
DevAyyub <ayyub.mammadov.2005@gmail.com>
2025-12-21 21:36:00 +0000

    Fix PEP8 type comparison

--- python-input_output/10-student.py
+++ python-input_output/10-student.py
@@ -24,7 +24,7 @@ class Student:
         list must be retrieved.
         Otherwise, all attributes must be retrieved.
         """
-        if (type(attrs) == list and
-                all(type(ele) == str for ele in attrs)):
+        if (isinstance(attrs, list) and
+                all(isinstance(ele, str) for ele in attrs)):
             return {k: getattr(self, k) for k in attrs if hasattr(self, k)}
         return self.__dict__
